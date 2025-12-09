145effb823162a533654812b7ac4c8a657d3e0ae
annabelxoxo <r1039110@student.thomasmore.be>
2025-12-09 19:14:12 +0100

    werkt dit nu gotverdomme????

---
+++ book_webshop_2XD/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <header>
+        <h1>hello world
+            
+        </h1>
+    </header>
+
+</body>
+</html>
